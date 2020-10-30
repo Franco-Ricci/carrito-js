372e8188d9a0efa78a382b2241c0a0f1b6cbe65f
Franco <francoricci_93@hotmail.com>
2020-10-30 11:51:19 -0300

    nuevo arreglo de objetos

--- carrito.js
+++ carrito.js
@@ -5,96 +5,111 @@ let libros = [
     id: 1,
     nombre: "Los guardianes",
     autor: "John Grisham",
-    descripcion:"Un hombre inocente fue condenado por asesinato hace veintidós años. Su abogado no parará hasta verle libre",
+    descripcion:
+      "Un hombre inocente fue condenado por asesinato hace veintidós años. Su abogado no parará hasta verle libre",
     precio: 1300,
     imagen: "images/rec-2.jpg",
     recomendados: true,
+    novedades: false,
   },
   {
     id: 2,
     nombre: "Las tinieblas y el alba",
     autor: "Ken Follet",
-    descripcion :"En Las tinieblas y el alba, Ken Follett embarca al lector en un épico viaje que termina en Los pilares de la Tierra comienza.",
+    descripcion:
+      "En Las tinieblas y el alba, Ken Follett embarca al lector en un épico viaje que termina en Los pilares de la Tierra comienza.",
     precio: 3795,
     imagen: "images/rec-1.png",
     recomendados: true,
+    novedades: false,
   },
   {
     id: 3,
     nombre: "Un lugar llamado antaño",
     autor: "Olga Tokarczuk",
-    descripcion: "Una novela mágica: la historia de un pueblo y sus excéntricos habitantes, que es al mismo tiempo la historia de un siglo y un país",
+    descripcion:
+      "Una novela mágica: la historia de un pueblo y sus excéntricos habitantes, que es al mismo tiempo la historia de un siglo y un país",
     precio: 1299,
     imagen: "images/rec-3.png",
     recomendados: true,
+    novedades: false,
   },
   {
     id: 4,
     nombre: "La tia cosima",
     autor: "Florencia Bonelli",
-    descripcion:"Cósima; un nombre para recordar. Una historia de cómo el amor vence al odio. La nueva novela de Florencia Bonelli; la escritora argentina más leída y admirada de América Latina ",
+    descripcion:
+      "Cósima; un nombre para recordar. Una historia de cómo el amor vence al odio. La nueva novela de Florencia Bonelli; la escritora argentina más leída y admirada de América Latina ",
     precio: 949,
     imagen: "images/rec-4.png",
     recomendados: true,
+    novedades: false,
   },
   {
     id: 5,
     nombre: "El Hombre Ilustrado",
     autor: "Ray Bradbury",
-    descripcion:"El narrador anónimo conoce a El Hombre Ilustrado, un curioso personaje con el cuerpo completamente cubierto de tatuajes. Sin embargo, lo más remarcable e inquietante es que las ilustraciones están mágicamente vivas.", 
+    descripcion:
+      "El narrador anónimo conoce a El Hombre Ilustrado, un curioso personaje con el cuerpo completamente cubierto de tatuajes. Sin embargo, lo más remarcable e inquietante es que las ilustraciones están mágicamente vivas.",
     precio: 2500,
     imagen: "images/nov-1.jpg",
     novedades: true,
+    recomendados: false,
   },
   {
     id: 6,
     nombre: "El enigma de la habitación 622",
     autor: "Joël Dicker",
-    descripcion :"Una noche de diciembre, un cadáver yace en el suelo de la habitación 622 del Palace de Verbier, un hotel de lujo en los Alpes suizos. La investigación policial no llegará nunca a término y el paso del tiempo hará que muchos olviden lo sucedido.",
+    descripcion:
+      "Una noche de diciembre, un cadáver yace en el suelo de la habitación 622 del Palace de Verbier, un hotel de lujo en los Alpes suizos. La investigación policial no llegará nunca a término y el paso del tiempo hará que muchos olviden lo sucedido.",
     precio: 2000,
     imagen: "images/nov-2.png",
     novedades: true,
+    recomendados: false,
   },
   {
     id: 7,
     nombre: "1984",
     autor: "George Orwell",
-    descripcion: "1984 es una de las novelas más inquietantes y atractivas del siglo XX. En el año 1984 Londres es una ciudad lúgubre en la que la Policía del Pensamiento controla de forma asfixiante la vida de los ciudadanos.",
+    descripcion:
+      "1984 es una de las novelas más inquietantes y atractivas del siglo XX. En el año 1984 Londres es una ciudad lúgubre en la que la Policía del Pensamiento controla de forma asfixiante la vida de los ciudadanos.",
     precio: 699,
     imagen: "images/nov-3.png",
     novedades: true,
+    recomendados: false,
   },
   {
     id: 8,
     nombre: "La Bailarina de Auschwitz",
     autor: "Eger Edhit Eva",
-    descripcion:"Una emocionante historia de superación sobre la capacidad del ser humano para sanar y vencer la adversidad. Un libro sobrecogedor, potente e inspirador que busca ayudar a todos aquellos cuyos traumas les impiden vivir en plenitud",
+    descripcion:
+      "Una emocionante historia de superación sobre la capacidad del ser humano para sanar y vencer la adversidad. Un libro sobrecogedor, potente e inspirador que busca ayudar a todos aquellos cuyos traumas les impiden vivir en plenitud",
     precio: 1250,
     imagen: "images/nov-4.png",
     novedades: true,
+    recomendados: false,
   },
 ];
-
 //----Creacion de data de productos, carrito, contador-------
-
 let productos = libros;
 let carrito = [];
 let contador = 0;
 let shoppingCartRec = document.getElementById("shoppingCart");
 document.getElementById("contador").innerHTML = contador;
-
 localStorage.setItem("productos", JSON.stringify(productos));
 let db = JSON.parse(localStorage.getItem("productos"));
 //--------------------------------------------------------------
 //Filtrar libros recomendados
 let tarjetas = document.getElementById("tarjetasRec");
-
-let librosrec = libros.filter(function (rec) {
-    return rec.recomendados;
-  });
-  //--------------------------------------------------------------
+// let librosrec = libros.filter(function (rec) {
+//     return rec.recomendados;
+//   });
+let librosrec = db.filter((item) => {
+  return item.recomendados === true;
+});
+//--------------------------------------------------------------
 //Creacion de cards
-librosrec.map(function (prod, i) {
+librosrec.map(function (prod) {
   let card = `
   <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5 mt-4 d-flex ">
   <div class="card mr-0 ml-0">
@@ -111,11 +126,11 @@ librosrec.map(function (prod, i) {
         <p style="text-align:left"> ${prod.descripcion} </p>
       </div>
       <span class="h2 text-center">$${prod.precio}</span>
-
+​
       <div class="row">
         <div class="col px-0 ml-0 trans text-center">
           <button type="submit"  class="btn btn btn-outline-danger">Comprar</button> 
-          <button type="submit" class="btn btn-outline-success" onclick='addCarrito(${i})'>Añadir a carrito</button>  
+          <button type="submit" class="btn btn-outline-success" onclick='addCarrito(${prod.id})'>Añadir a carrito</button>  
         </div>
       </div>
     </div>
@@ -124,16 +139,16 @@ librosrec.map(function (prod, i) {
 </div>`;
   tarjetas.innerHTML += card;
 });
-
-
 //--------------------------------------------------------------
 //Filtrar libros Novedades
 let tarjetasNov = document.getElementById("tarjetasNov");
-
-let librosNov = libros.filter(function (nov) {
-    return nov.novedades;
-  });
-librosNov.map(function (prod, i) {
+// let librosNov = libros.filter(function (nov) {
+//   return nov.novedades;
+// });
+let librosNov = db.filter((item) => {
+  return item.novedades === true;
+});
+librosNov.map(function (prod) {
   let card = `
   <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5 mt-4 d-flex ">
   <div class="card mr-0 ml-0">
@@ -150,11 +165,11 @@ librosNov.map(function (prod, i) {
         <p style="text-align:left"> ${prod.descripcion} </p>
       </div>
       <span class="h2 text-center">$${prod.precio}</span>
-
+​
       <div class="row">
         <div class="col px-0 ml-0 trans text-center">
           <button type="submit"  class="btn btn btn-outline-danger">Comprar</button> 
-          <button type="submit" class="btn btn-outline-success" onclick='addCarrito(${i})'>Añadir a carrito</button>  
+          <button type="submit" class="btn btn-outline-success" onclick='addCarrito(${prod.id})'>Añadir a carrito</button>  
         </div>
       </div>
     </div>
@@ -163,11 +178,13 @@ librosNov.map(function (prod, i) {
 </div>`;
   tarjetasNov.innerHTML += card;
 });
-
 function addCarrito(index) {
-  let producto = db[index];
-
-   console.log(producto);
+  let buscar = db.find((item) => {
+    return item.id === index;
+  });
+  let producto = buscar;
+  // let producto = db[index];
+  console.log(producto);
   let storage = JSON.parse(localStorage.getItem("carrito")) || []; //si no existe la clave me devuelva un arreglo vacio
   storage.push(producto);
   localStorage.setItem("carrito", JSON.stringify(storage));
